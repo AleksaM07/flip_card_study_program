c7c93639238b66af22d986f3f1f7e3aad06cc8df
AleksaM07 <aleksam07@gmail.com>
2023-03-08 19:59:59 +0100

    adding comments

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import tkinter.scrolledtext
+import tkinter.scrolledtext #for questions and answers that are a bit longer
 from tkinter import messagebox
-from tkinter import *
-import pandas, random, atexit
+from tkinter import * #for making the GUI
+import pandas, random, atexit #atexit we used to save the file before exit
 from PIL import Image, ImageTk
 
 
@@ -12,37 +12,35 @@ QUESTION = "Pitanje"
 ANSWER ="Odgovor"
 current_question_index = 0
 current_question = ""
-
 i = 0
 
-#--------------------pick a random question funktion-----------------------------
+#--------------------pick a random question function-----------------------------
 def extract_question():
-    global lista
+    global lista #we are taking a saved list of questions, or we start a new
     data = pandas.read_csv("data/ORK pitanja.csv", encoding='Windows-1252')
     rows = data["Pitanje"].to_dict()
     index = random.choice(lista)
     question = rows[index]
     return question, index
 
+#this function is for when we choose the cross, taht is that we dont know the answer
 def get_questions_red():
     global current_question_index, current_question, lista
     current_question, current_question_index = extract_question()
-    question_and_answer.configure(state='normal')
+    question_and_answer.configure(state='normal') #we need to set the state of the tkinter.scrolledtext to noraml so we can edit it
     question_and_answer.delete("1.0", "end")
-    question_and_answer.tag_configure('center', justify='center')
+    question_and_answer.tag_configure('center', justify='center') #we configured what "center" tag means for the tkinter.scrolledtext
     question_and_answer.insert("end", current_question, 'center')
-    question_and_answer.configure(state='disabled')
+    question_and_answer.configure(state='disabled') #we need to set the state of the tkinter.scrolledtext to disabled so we can't edit it manualy
 
     canvas.itemconfig(canvas_picture, image=card_front)
     question_and_answer.config(background="white")
     canvas.itemconfig(card_title, text=QUESTION)
-    print(lista)
 
 def get_questions_green():
     global lista, current_question_index
     get_questions_red()
     lista.remove(current_question_index)
-    print(lista)
 
 #--------------------flip the side of the card to answer-----------------------------
 def extract_answer():
@@ -75,14 +73,13 @@ def get_answer():
         question_and_answer.tag_configure('center', justify='center')
         question_and_answer.insert("end", current_question, 'center')
         question_and_answer.configure(state='disabled')
-    i += 1
-#---------------------------------------------------------------------------------
+    i += 1  #we change the i so we can keep the track of if the card is flipped or not, if its even it isnt flipped
 
 #before we exit------------------------------------------------
 def exit_handler():
     global lista
     data = pandas.DataFrame(lista)
-    data.to_csv("data/saved_progress", index=False)
+    data.to_csv("data/saved_progress", index=False) #we're jsut saving the list so if the user want to use it again
 atexit.register(exit_handler)
 #---------------------------------------------------------------
 
@@ -98,9 +95,12 @@ card_front = PhotoImage(file='images/card_front.png')
 right = PhotoImage(file='images/right.png')
 wrong = PhotoImage(file='images/wrong.png')
 
-#In Python, objects are garbage collected when there are no more references to them. When an object is garbage collected, its memory is freed and it is no longer available to the program.
-#In the case of Tkinter widgets that display images, the image data is stored in a PhotoImage object. If there are no references to the PhotoImage object, it will be garbage collected, and the widget that references it will no longer display the image.
-#Therefore, to ensure that the image is displayed correctly in a Tkinter widget, you need to keep a reference to the PhotoImage object for as long as the widget is being displayed. This can be achieved by assigning the PhotoImage object to an attribute of a widget, such as the image attribute of a Label, or by storing it in a global variable or an instance variable of a class. By doing so, you ensure that the PhotoImage object is not garbage collected until it is no longer needed.
+#In Python, objects are garbage collected when there are no more references to them.
+#In the case of Tkinter widgets that display images, the image data is stored in a PhotoImage object.
+#If there are no references to the PhotoImage object, it will be garbage collected, and the widget that references it will no longer display the image.
+#Therefore, to ensure that the image is displayed correctly in a Tkinter widget,
+#we need to keep a reference to the PhotoImage object for as long as the widget is being displayed.
+# This can be achieved by assigning the PhotoImage object to an attribute of a widget, such as the image attribute of a Label, or by storing it in a global variable or an instance variable of a class. By doing so, you ensure that the PhotoImage object is not garbage collected until it is no longer needed.
 flip_resized = Image.open('images/flip.png').resize((130,100))
 flip = ImageTk.PhotoImage(flip_resized)
 
@@ -127,7 +127,7 @@ if is_ok:
         lista = list(unedited_list.keys())
         print(lista)
 else:
-    try:
+    try: #we need to check if there is even a file to be opened
         with open("data/saved_progress.csv", mode="r") as data:
             lista = data.read()
     except FileNotFoundError:
